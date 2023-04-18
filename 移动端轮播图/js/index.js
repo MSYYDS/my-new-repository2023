d35659d4d1ae5943e62b4e4f4d6e49de31692a65
ms <3128363022@qq.com>
2023-04-18 20:02:50 +0800

    反复练习轮播图写法

--- 移动端轮播图/js/index.js
+++ 移动端轮播图/js/index.js
@@ -3,14 +3,15 @@ if ('addEventListener' in document) {
         FastClick.attach(document.body);
     }, false);
 }
+// 第三次工作，我不信了，呜呜
+//首先是浏览器加载执行
 window.addEventListener('load', function () {
-    var focus = document.querySelector('.focus');
-    var ul = focus.querySelector('ul');
-    var ol = focus.querySelector('ol');
+    //轮播图自动播放
+    var focus = this.document.querySelector('.focus');
+    var ul = focus.children[0];
+    var ol = focus.children[1];
+    var w = ul.children[0].offsetWidth;
     var index = 0;
-    var w = focus.offsetWidth;
-
-    //首先自动轮播
     var timer = setInterval(function () {
         index++;
         var translatex = -index * w;
@@ -18,45 +19,49 @@ window.addEventListener('load', function () {
         ul.style.transform = 'translateX(' + translatex + 'px)';
     }, 2000);
 
-    //判断index，无缝循环
+    //每次动完我再判断
     ul.addEventListener('transitionend', function () {
         if (index >= 3) {
             index = 0;
-            ul.style.transition = 'none';
             var translatex = -index * w;
+            ul.style.transition = 'none';
             ul.style.transform = 'translateX(' + translatex + 'px)';
-
         } else if (index < 0) {
             index = 2;
-            ul.style.transition = 'none';
             var translatex = -index * w;
+            ul.style.transition = 'none';
+            ul.style.transform = 'translateX(' + translatex + 'px)';
+        } else {
+            var translatex = -index * w;
+            ul.style.transition = 'all .3s';
             ul.style.transform = 'translateX(' + translatex + 'px)';
         }
 
-
-        //小圆点跟随移动
         ol.querySelector('.current').classList.remove('current');
         ol.children[index].classList.add('current');
+
     })
 
-    var moveX = 0;
-    var x = 0;
-    //图片跟随触摸点移动
+    //开始触摸事件
+    var startX = 0;
     ul.addEventListener('touchstart', function (e) {
         clearInterval(timer);
-        x = e.targetTouches[0].pageX;
+        startX = e.targetTouches[0].pageX;
     })
 
+
+    //触摸滑动事件，图片跟着动
     ul.addEventListener('touchmove', function (e) {
-        moveX = e.targetTouches[0].pageX - x;
-        ul.style.transition = 'none';
+        moveX = e.targetTouches[0].pageX - startX;
         var translatex = -index * w + moveX;
+        ul.style.transition = 'none';
         ul.style.transform = 'translateX(' + translatex + 'px)';
         flag = true;
         e.preventDefault();
     })
 
-    var flag = true;//节流阀，哈哈哈，我自己想的，哇嘎嘎嘎；
+    //触摸结束事件，图片滑到到某个位置
+    var flag = true;//节流阀
     ul.addEventListener('touchend', function () {
         if (flag) {
             flag = false;
@@ -64,34 +69,31 @@ window.addEventListener('load', function () {
                 if (moveX > 0) {
                     index--;
                 } else {
-                    index++;
+                    index++
                 }
-                var translatex = -index * w;
-                ul.style.transition = 'all .3s';
-                ul.style.transform = 'translateX(' + translatex + 'px)';
-            } else {
                 var translatex = -index * w;
                 ul.style.transition = 'all .1s';
                 ul.style.transform = 'translateX(' + translatex + 'px)';
             }
         }
 
+        //开启定时器；
         clearInterval(timer);
         timer = setInterval(function () {
             index++;
             var translatex = -index * w;
             ul.style.transition = 'all .3s';
             ul.style.transform = 'translateX(' + translatex + 'px)';
-        }, 2000)
+        }, 2000);
     })
 
+    //返回顶部之作
     var goBack = document.querySelector('.goBack');
     var nav = document.querySelector('nav');
-    var search_index = document.querySelector('.search-index');
-    var search_index_Top = search_index.offsetHeight;
     var navTop = nav.offsetTop;
+
     document.addEventListener('scroll', function () {
-        if (window.pageYOffset >= navTop - search_index_Top) {
+        if (window.pageYOffset >= navTop) {
             goBack.style.display = 'block';
         } else {
             goBack.style.display = 'none';
@@ -100,12 +102,269 @@ window.addEventListener('load', function () {
 
     goBack.addEventListener('click', function () {
         window.scroll(0, 0);
-
     })
-
-
-
 })
+
+
+
+
+
+
+
+
+
+
+// //开始第二次工作，对！工作;好写完了，我傻了
+// //首先是浏览器加载完成后执行
+// window.addEventListener('load', function () {
+//     //选中目标先做自动切换；并且小圆点自动跟随变化
+//     var focus = document.querySelector('.focus');
+//     var ul = focus.children[0];
+//     var ol = focus.children[1];
+//     //添加一个定时器
+//     var w = ul.children[0].offsetWidth;
+//     // console.log(w);
+//     var index = 0;
+//     var timer = setInterval(function () {
+//         index++;
+//         var translatex = -index * w;
+//         ul.style.transition = 'all .3s';
+//         ul.style.transform = 'translateX(' + translatex + 'px)';
+//     }, 2000)
+
+//     //每次页面停止后判断
+//     ul.addEventListener('transitionend', function () {
+//         if (index >= 3) {
+//             index = 0;
+//         } else if (index < 0) {
+//             index = 2;
+//         }
+//         var translatex = -index * w;
+//         ul.style.transition = 'none';
+//         ul.style.transform = 'translateX(' + translatex + 'px)';
+
+//         ol.querySelector('.current').classList.remove('current'); //移除属性不会写；
+//         ol.children[index].classList.add('current');
+//     })
+
+
+//     //手指触摸事件
+//     ul.addEventListener('touchstart', function (e) {
+//         clearInterval(timer);
+//         startX = e.targetTouches[0].pageX;
+//     })
+
+//     //手指滑动事件
+//     ul.addEventListener('touchmove', function (e) {
+//         moveX = e.targetTouches[0].pageX - startX;
+//         var translatex = -index * w + moveX;
+//         ul.style.transition = 'none';
+//         ul.style.transform = 'translateX(' + translatex + 'px)';
+//         e.preventDefault();
+//         flag = true;
+//     })
+
+
+//     //手指结束事件
+//     var flag = true;
+//     ul.addEventListener('touchend', function () {
+//         if (flag) {
+//             flag = false;
+//             if (Math.abs(moveX) > 50) {
+//                 if (moveX > 0) {
+//                     index--;
+//                 } else if (moveX < 0) {
+//                     index++;
+//                 }
+//                 var translatex = -index * w;
+//                 ul.style.transition = 'all .3s';
+//                 ul.style.transform = 'translateX(' + translatex + 'px)';
+//             } else {
+//                 var translatex = -index * w;
+//                 ul.style.transition = 'all .1s';
+//                 ul.style.transform = 'translateX(' + translatex + moveX + 'px)';
+//             }
+//         }
+
+
+//         //手指停止触摸开启定时器；
+//         clearInterval(timer);
+//         timer = setInterval(function () {
+//             index++;
+//             var translatex = -index * w;
+//             ul.style.transition = 'all .3s';
+//             ul.style.transform = 'translateX(' + translatex + 'px)';
+//         }, 2000)
+//     })
+
+//     //返回顶部
+//     var nav = document.querySelector('nav');
+//     var navTop = nav.offsetTop;
+//     var goBack = document.querySelector('.goBack');
+
+//     document.addEventListener('scroll', function () {
+//         if (window.pageYOffset >= navTop) {
+//             goBack.style.display = 'block';
+//         } else {
+//             goBack.style.display = 'none';
+//         }
+//     })
+
+//     goBack.addEventListener('click', function () {
+//         window.scroll(0, 0);
+//     })
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //第一次写，速度好慢，因为不熟练，而且需要理解
+// window.addEventListener('load', function () {
+//     var focus = document.querySelector('.focus');
+//     var ul = focus.querySelector('ul');
+//     var ol = focus.querySelector('ol');
+//     var index = 0;
+//     var w = focus.offsetWidth;
+
+//     //首先自动轮播
+//     var timer = setInterval(function () {
+//         index++;
+//         var translatex = -index * w;
+//         ul.style.transition = 'all .3s';
+//         ul.style.transform = 'translateX(' + translatex + 'px)';
+//     }, 2000);
+
+//     //判断index，无缝循环
+//     ul.addEventListener('transitionend', function () {
+//         if (index >= 3) {
+//             index = 0;
+//             ul.style.transition = 'none';
+//             var translatex = -index * w;
+//             ul.style.transform = 'translateX(' + translatex + 'px)';
+
+//         } else if (index < 0) {
+//             index = 2;
+//             ul.style.transition = 'none';
+//             var translatex = -index * w;
+//             ul.style.transform = 'translateX(' + translatex + 'px)';
+//         }
+
+
+//         //小圆点跟随移动
+//         ol.querySelector('.current').classList.remove('current');
+//         ol.children[index].classList.add('current');
+//     })
+
+//     var moveX = 0;
+//     var x = 0;
+//     //图片跟随触摸点移动
+//     ul.addEventListener('touchstart', function (e) {
+//         clearInterval(timer);
+//         x = e.targetTouches[0].pageX;
+//     })
+
+//     ul.addEventListener('touchmove', function (e) {
+//         moveX = e.targetTouches[0].pageX - x;
+//         ul.style.transition = 'none';
+//         var translatex = -index * w + moveX;
+//         ul.style.transform = 'translateX(' + translatex + 'px)';
+//         flag = true;
+//         e.preventDefault();
+//     })
+
+//     var flag = true;//节流阀，哈哈哈，我自己想的，哇嘎嘎嘎；
+//     ul.addEventListener('touchend', function () {
+//         if (flag) {
+//             flag = false;
+//             if (Math.abs(moveX) > 50) {
+//                 if (moveX > 0) {
+//                     index--;
+//                 } else {
+//                     index++;
+//                 }
+//                 var translatex = -index * w;
+//                 ul.style.transition = 'all .3s';
+//                 ul.style.transform = 'translateX(' + translatex + 'px)';
+//             } else {
+//                 var translatex = -index * w;
+//                 ul.style.transition = 'all .1s';
+//                 ul.style.transform = 'translateX(' + translatex + 'px)';
+//             }
+//         }
+
+//         clearInterval(timer);
+//         timer = setInterval(function () {
+//             index++;
+//             var translatex = -index * w;
+//             ul.style.transition = 'all .3s';
+//             ul.style.transform = 'translateX(' + translatex + 'px)';
+//         }, 2000)
+//     })
+
+//     var goBack = document.querySelector('.goBack');
+//     var nav = document.querySelector('nav');
+//     var search_index = document.querySelector('.search-index');
+//     var search_index_Top = search_index.offsetHeight;
+//     var navTop = nav.offsetTop;
+//     document.addEventListener('scroll', function () {
+//         if (window.pageYOffset >= navTop - search_index_Top) {
+//             goBack.style.display = 'block';
+//         } else {
+//             goBack.style.display = 'none';
+//         }
+//     })
+
+//     goBack.addEventListener('click', function () {
+//         window.scroll(0, 0);
+
+//     })
+
+
+
+// })
 
 
 
